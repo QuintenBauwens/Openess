cff5527cc899f6c66826dc1aeb60ac7202b8ac80
Quinten Bauwens <QBAUWENS@volvocars.com>
2024-09-12 10:48:44 +0200

    Adjust tree

--- src/main.py
+++ src/main.py
@@ -12,8 +12,7 @@ def main(root):
 		update_logger_config(settings.get('logger', {}))
 		logger.info(f"Lanching application with settings: {settings}")
 	except Exception as e:
-		print("Launching application has been aborted due to an error: ", e)
-		logger.warning(e, exc_info=True)
+		logger.warning("Launching application has been aborted due to an error: ", e, exc_info=True)
 	return settings
 
 if __name__ == "__main__":
